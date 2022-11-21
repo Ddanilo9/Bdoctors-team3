6dcdd6284b5b5e176c0dba3342fdc950004213c2
Albiddd <alberto.dore93@gmail.com>
2022-11-21 16:04:02 +0100

    add spce name fillable

--- app/Specialization.php
+++ app/Specialization.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Specialization extends Model
 {
+
+    protected $fillable = [
+        'spec_name',
+    ];
+
     public function doctors()
     {
         return $this->belongsToMany('App\Doctor');
