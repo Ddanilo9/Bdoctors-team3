5492f135bb7101cd16c34e68d6a8035f58ec6d49
Sansone11 <sansonemirco@gmail.com>
2022-11-22 10:44:57 +0100

    test

--- public/js/app.js
+++ public/js/app.js
@@ -49729,8 +49729,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Danilo\Desktop\Bdoctors-team3\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Danilo\Desktop\Bdoctors-team3\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\sanso\Saved Games\Desktop\Bdoctors-team3\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\sanso\Saved Games\Desktop\Bdoctors-team3\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
