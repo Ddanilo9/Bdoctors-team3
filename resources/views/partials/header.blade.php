5b7b4766509faa08d42a97f67f1f3563f5d5d773
manyblake <manyblake@gmail.com>
2022-11-23 15:51:07 +0100

    new dashboard

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -36,7 +36,7 @@
                                         {{ __('Profilo privato') }}
                                     </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
