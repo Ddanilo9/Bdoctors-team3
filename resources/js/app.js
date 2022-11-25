e7b7df24381f96503dcc4b8e753099a237e820e4
manyblake <manyblake@gmail.com>
2022-11-25 09:58:53 +0100

    add Carousel component

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,15 +4,13 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-window.axios = require('axios');
+window.axios = require("axios");
 
+require("./bootstrap");
 
-require('./bootstrap');
+window.Vue = require("vue");
 
-window.Vue = require('vue');
-
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -24,8 +22,11 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-Vue.component('carousel', require('./components/Carousel').default);
+Vue.component(
+    "example-component",
+    require("./components/ExampleComponent.vue").default
+);
+Vue.component("carousel", require("./components/Carousel").default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -33,9 +34,7 @@ Vue.component('carousel', require('./components/Carousel').default);
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-
- const app = new Vue({
-    el: '#app',
+const app = new Vue({
+    el: "#app",
     // render: h => h(App),
-    
 });
