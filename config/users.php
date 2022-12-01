e34eb57153233fa146e57e6e3014caa7a22c1a05
Ddanilo9 <interiordanilo3@gmail.com>
2022-12-01 15:38:23 +0100

    fix seeder

---
+++ config/users.php
@@ -0,0 +1,201 @@
+<?php
+
+return [
+    [
+        'email'=>'milomarziali@dottori.com',
+        'password'=>'doctors59',
+    ],
+    [
+        'email'=>'cristianpellegrini@dottori.com',
+        'password'=>'doctors59',
+    ],
+    [
+        'email'=>'marcozampieri@dottori.com',
+        'password'=>'doctors59',
+    ],
+    [
+        'email'=>'vilmamaria@dottori.com',
+        'password'=>'doctors59',
+    ],
+    [
+        'email'=>'giorgiofrancuzzi@dottori.com',
+        'password'=>'doctors59',
+    ],
+    [
+        'email'=>'martinaseravelli@dottori.com',
+        'password'=>'doctors59',
+      
+    ],
+    [
+        'email'=>'mariarosaromeo@dottori.com',
+        'password'=>'doctors59',
+       
+    ],
+    [
+        'email'=>'sergiopiombino@dottori.com',
+        'password'=>'doctors59',
+    
+    ],
+    [
+        'email'=>'carlarando@dottori.com',
+        'password'=>'doctors59',
+      
+    ],
+    [
+        'email'=>'chiarapirozzi@dottori.com',
+        'password'=>'doctors59',
+     
+    ],
+    [
+        'email'=>'giorgiamarconi@dottori.com',
+        'password'=>'doctors59',
+      
+    ],
+    [
+        'email'=>'simonerossi@dottori.com',
+        'password'=>'doctors59',
+      
+    ],
+    [
+        'email'=>'giorgiograsso@dottori.com',
+        'password'=>'doctors59',
+        
+    ],
+    [
+        'email'=>'sarapanebianco@dottori.com',
+        'password'=>'doctors59',
+      
+    ],
+    [
+        'email'=>'giorgiovalli@dottori.com',
+        'password'=>'doctors59',
+        
+    ],
+    [
+        'email'=>'marcolori@dottori.com',
+        'password'=>'doctors59',
+ 
+    ],
+    [
+        'email'=>'mariacarlaserafini@dottori.com',
+        'password'=>'doctors59',
+   
+    ],
+    [
+        'email'=>'francoscoglio@dottori.com',
+        'password'=>'doctors59',
+      
+    ],
+    [
+        'email'=>'giuseppecalogero@dottori.com',
+        'password'=>'doctors59',
+       
+    ],
+    [
+        'email'=>'antoniomirulla@dottori.com',
+        'password'=>'doctors59',
+   
+    ],
+    [
+        'email'=>'orianafranchi@dottori.com',
+        'password'=>'doctors59',
+     
+    ],
+    [
+        'email'=>'alessandrogiorgi@dottori.com',
+        'password'=>'doctors59',
+       
+    ],
+    [
+        'email'=>'elenadenici@dottori.com',
+        'password'=>'doctors59',
+       
+    ],
+    [
+        'email'=>'rossanasaruzzi@dottori.com',
+        'password'=>'doctors59',
+        
+    ],
+    [
+        'email'=>'marcopalmara@dottori.com',
+        'password'=>'doctors59',
+       
+    ],
+    [
+        'email'=>'carlottamarmora@dottori.com',
+        'password'=>'doctors59',
+       
+    ],
+    [
+        'email'=>'biagioleori@dottori.com',
+        'password'=>'doctors59',
+     
+    ],
+    [
+        'email'=>'paolomeneguzzi@dottori.com',
+        'password'=>'doctors59',
+        
+    ],
+    [
+        'email'=>'francescabartolini@dottori.com',
+        'password'=>'doctors59',
+      
+    ],
+    [
+        'email'=>'giuliarovere@dottori.com',
+        'password'=>'doctors59',
+      
+    ],
+    [
+        'email'=>'bernardodalia@dottori.com',
+        'password'=>'doctors59',
+       
+    ],
+    [
+        'email'=>'emanueleallegra@dottori.com',
+        'password'=>'doctors59',
+       
+    ],
+    [
+        'email'=>'simonademeo@dottori.com',
+        'password'=>'doctors59',
+        
+    ],
+    [
+        'email'=>'carlaminici@dottori.com',
+        'password'=>'doctors59',
+      
+    ],
+    [
+        'email'=>'alessandradantoni@dottori.com',
+        'password'=>'doctors59',
+      
+    ],
+    [
+        'email'=>'silviaformini@dottori.com',
+        'password'=>'doctors59',
+       
+    ],
+    [
+        'email'=>'francescobottari@dottori.com',
+        'password'=>'doctors59',
+      
+    ],
+    [
+        'email'=>'marcovieri@dottori.com',
+        'password'=>'doctors59',
+       
+    ],
+    [
+        'email'=>'emilynovik@dottori.com',
+        'password'=>'doctors59',
+   
+    ],
+    [
+        'email'=>'oraziosordi@dottori.com',
+        'password'=>'doctors59',
+  
+    ],
+]
+
+?>
