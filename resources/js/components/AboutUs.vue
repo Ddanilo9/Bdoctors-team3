c007277a8176eb16e3414469f85444e284d8f828
manyblake <manyblake@gmail.com>
2022-12-02 10:30:45 +0100

    change images path

--- resources/js/components/AboutUs.vue
+++ resources/js/components/AboutUs.vue
@@ -1,37 +1,48 @@
 <template>
-  <div class="about-us">
-    
-    
-    <div class="container-about">
-      <div class="container">
-        <h1 class="text-center">Dicono di Noi</h1>
-      </div>
-      <p>Oltre il 99% di pazienti soddisfatti</p>
-      <p>BDoctors è il primo sito in Italia di <strong>prenotazioni</strong> di <strong>visite mediche</strong> ed esami diagnostici, online <strong>dal 2022</strong>.</p>
-      <p>Crediamo nell'importanza dell'<strong>informazione</strong> e della <strong>trasparenza</strong> per aiutare i pazienti a <strong>scegliere</strong> il <strong>medico giusto</strong> tra gli oltre 9.000 presenti su BDoctors.</p>
-      <p>Per questo su BDoctors, solo i pazienti che hanno prenotato attraverso il sito e svolto la prestazione possono rilasciare un feedback sul medico: una <strong>garanzia d'affidabilità</strong> è data dalle <strong>oltre 100.000 recensioni</strong> che leggi.</p>
-      
+    <div class="about-us">
+        <div class="container-about">
+            <div class="container">
+                <h1 class="text-center">Dicono di Noi</h1>
+            </div>
+            <p>Oltre il 99% di pazienti soddisfatti</p>
+            <p>
+                BDoctors è il primo sito in Italia di
+                <strong>prenotazioni</strong> di
+                <strong>visite mediche</strong> ed esami diagnostici, online
+                <strong>dal 2022</strong>.
+            </p>
+            <p>
+                Crediamo nell'importanza dell'<strong>informazione</strong> e
+                della <strong>trasparenza</strong> per aiutare i pazienti a
+                <strong>scegliere</strong> il <strong>medico giusto</strong> tra
+                gli oltre 9.000 presenti su BDoctors.
+            </p>
+            <p>
+                Per questo su BDoctors, solo i pazienti che hanno prenotato
+                attraverso il sito e svolto la prestazione possono rilasciare un
+                feedback sul medico: una
+                <strong>garanzia d'affidabilità</strong> è data dalle
+                <strong>oltre 100.000 recensioni</strong> che leggi.
+            </p>
+        </div>
     </div>
-    
-  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-
 <style lang="scss" scoped>
-h1{
-  color: #95BBDF;
+h1 {
+    color: #95bbdf;
     font-size: 35px;
 }
-.about-us{
+.about-us {
     background-image: url(../../../public/img/aa.png);
     background-position: top;
     background-repeat: no-repeat;
     background-size: cover;
-    border-top: 2px solid #95BBDF;
+    border-top: 2px solid #95bbdf;
     height: 500px;
     display: flex;
     align-items: center;
@@ -39,7 +50,7 @@ h1{
     justify-content: center;
     width: 100%;
 }
-.container-about{
+.container-about {
     max-width: 85%;
     padding: 15px;
     margin: 0 auto;
@@ -51,18 +62,18 @@ h1{
 }
 
 @media (min-width: 425px) {
-        .container-about{
-            padding: 20px 25px;
-        }
+    .container-about {
+        padding: 20px 25px;
     }
+}
 @media (min-width: 768px) {
-        // .container-about{
-            
-        // }
-    }
+    // .container-about{
+
+    // }
+}
 @media (min-width: 1200px) {
-    .container-about{
+    .container-about {
         max-width: 750px;
-        }
+    }
 }
 </style>
--- resources/js/components/DoctorCard.vue
+++ resources/js/components/DoctorCard.vue
@@ -1,47 +1,60 @@
 <template>
-  <div class="box-card">
-    <div class="card-1" :class="styleModifier">
-      <div class="card-1-header">
-        <h4 class="text-center py-2">{{ doctor.name }} {{ doctor.surname }}</h4>
-        <figure>
-          <img :src="`/storage/${doctor.photo}`" alt="doctor avatar" />
-        </figure>
-      </div>
-      <div class="card-1-body ">
-        <div class="mb-1" v-for="spec in doctor.specializations" :key="spec.id">
-          <span class="tag tag-teal">{{ spec.spec_name }}</span>
-        </div>
-        <div class="stars mt-1 mb-1">
-          <template v-for="i in 5">
-            <template v-if="i <= Math.round(doctor.avgRate)">
-              <font-awesome-icon
-                :key="'fs' + i"
-                icon="fa-solid fa-star"
-                class="full-star"
-              />
-            </template>
-            <template v-else>
-              <font-awesome-icon :key="'es' + i" icon="fa-regular fa-star" />
-            </template>
-          </template>
-        </div>
-
-        <div class="only-show">
-          <p>
-            n° di reviews:
-            <span>({{ doctor.reviews.length }})</span>
-          </p>
-          <div class="user">
-            <div class="user-info d-flex align-items-center">
-              <img :src="require('../../images/maps.png')" />
-              <h5>{{ doctor.address }}</h5>
+    <div class="box-card">
+        <div class="card-1" :class="styleModifier">
+            <div class="card-1-header">
+                <h4 class="text-center py-2">
+                    {{ doctor.name }} {{ doctor.surname }}
+                </h4>
+                <figure>
+                    <img
+                        :src="`/storage/${doctor.photo}`"
+                        alt="doctor avatar"
+                    />
+                </figure>
             </div>
-          </div>
-        </div>
+            <div class="card-1-body">
+                <div
+                    class="mb-1"
+                    v-for="spec in doctor.specializations"
+                    :key="spec.id"
+                >
+                    <span class="tag tag-teal">{{ spec.spec_name }}</span>
+                </div>
+                <div class="stars mt-1 mb-1">
+                    <template v-for="i in 5">
+                        <template v-if="i <= Math.round(doctor.avgRate)">
+                            <font-awesome-icon
+                                :key="'fs' + i"
+                                icon="fa-solid fa-star"
+                                class="full-star"
+                            />
+                        </template>
+                        <template v-else>
+                            <font-awesome-icon
+                                :key="'es' + i"
+                                icon="fa-regular fa-star"
+                            />
+                        </template>
+                    </template>
+                </div>
 
-      </div>
+                <div class="only-show">
+                    <p>
+                        n° di reviews:
+                        <span>({{ doctor.reviews.length }})</span>
+                    </p>
+                    <div class="user">
+                        <div class="user-info d-flex align-items-center">
+                            <img
+                                :src="require('../../../public/img/maps.png')"
+                            />
+                            <h5>{{ doctor.address }}</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -71,115 +84,112 @@ export default {
 <style lang="scss" scoped>
 @import "../../sass/variables.scss";
 
-
 * {
-  box-sizing: border-box;
+    box-sizing: border-box;
 }
 
 .box-card {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
 }
 .card-1 {
-  background-color: #fff;
-  border-radius: 10px;
-  box-shadow: 0 2px 20px rgba(0, 0, 0, 0.2);
-  overflow: hidden;
-  width: 300px;
-  margin-bottom: 80px;
-  h4{
-    font-size: 30px;
-  }
+    background-color: #fff;
+    border-radius: 10px;
+    box-shadow: 0 2px 20px rgba(0, 0, 0, 0.2);
+    overflow: hidden;
+    width: 300px;
+    margin-bottom: 80px;
+    h4 {
+        font-size: 30px;
+    }
 }
 .card-1-header img {
-  width: 100%;
-  object-fit: cover;
+    width: 100%;
+    object-fit: cover;
 }
 .card-1-body {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
-  padding: 20px;
-  min-height: 250px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    padding: 20px;
+    min-height: 250px;
 }
 
 .tag {
-  background: #cccccc;
-  border-radius: 50px;
-  font-size: 14px;
-  margin: 0;
-  color: #fff;
-  padding: 2px 10px;
-  text-transform: uppercase;
-  cursor: pointer;
+    background: #cccccc;
+    border-radius: 50px;
+    font-size: 14px;
+    margin: 0;
+    color: #fff;
+    padding: 2px 10px;
+    text-transform: uppercase;
+    cursor: pointer;
 }
 .tag-teal {
-  background-color: #47bcd4;
+    background-color: #47bcd4;
 }
 .tag-purple {
-  background-color: #5e76bf;
+    background-color: #5e76bf;
 }
 .tag-pink {
-  background-color: #cd5b9f;
+    background-color: #cd5b9f;
 }
 
 .card-1-body p {
-  font-size: 14px;
-  margin: 0 0 40px;
+    font-size: 14px;
+    margin: 0 0 40px;
 }
 .user {
-  display: flex;
-  margin-bottom: -16px;
+    display: flex;
+    margin-bottom: -16px;
 }
 
 .user img {
-  border-radius: 50%;
-  width: 40px;
-  height: 40px;
-  margin-right: 10px;
+    border-radius: 50%;
+    width: 40px;
+    height: 40px;
+    margin-right: 10px;
 }
 .user-info h5 {
-  font-size: 14px;
-  margin: 0;
+    font-size: 14px;
+    margin: 0;
 }
 .full-star {
-  color: yellow;
+    color: yellow;
 }
 .user-info small {
-  color: #545d7a;
+    color: #545d7a;
 }
 figure {
-  margin: 5px auto;
-  aspect-ratio: 1/1;
-  overflow: hidden;
-  padding: 1px;
+    margin: 5px auto;
+    aspect-ratio: 1/1;
+    overflow: hidden;
+    padding: 1px;
 
-  img {
-    width: 100%;
-    object-fit: cover;
-    object-position: center;
-  }
+    img {
+        width: 100%;
+        object-fit: cover;
+        object-position: center;
+    }
 }
 
-
 .carousel__doctor {
-  width: 100%;
-  margin: 0;
-  padding: 32px 64px;
-  background-color: $bd-polar;
-  height: 100%;
+    width: 100%;
+    margin: 0;
+    padding: 32px 64px;
+    background-color: $bd-polar;
+    height: 100%;
 
-  .card-1-body {
+    .card-1-body {
+        min-height: 0;
+    }
 
-    min-height: 0;
-  }
-
-  .only-show {
-    display: none;
-  }
-  }
+    .only-show {
+        display: none;
+    }
+}
 </style>
